3ce218160395efe206f18ad172101d72220ce3f8
Nicole <lancasternj@gmail.com>
2023-03-28 18:18:12 +0100

    realising there was a JS NaN method so refactoring based on this

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,11 +1,9 @@
 const articles = require("../db/data/test-data/articles.js");
 const { fetchArticlesById } = require("../models/articles.models.js");
-const { customErrorMiddleware } = require("./error.controllers.js");
 
 const getArticlesById = (request, response, next) => {
   const { article_id } = request.params;
-  const isNum = Number(article_id);
-  if (!isNum && isNum !== 0) {
+  if (isNaN(article_id)) {
     const err = { status: 400, msg: "Article ID invalid - must be a number" }
     next(err)
   }
