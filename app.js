4954524439193c9733d2303cb1575266f8e04788
Nicole <lancasternj@gmail.com>
2023-04-17 10:32:45 +0100

    enabling CORS

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const { getAllEndpoints } = require("./controllers/api.controller.js")
+const { getAllEndpoints } = require("./controllers/api.controller.js");
 const { getTopics } = require("./controllers/topics.controllers.js");
-const { getUsers } = require('./controllers/users.controllers.js')
+const { getUsers } = require("./controllers/users.controllers.js");
 const {
   getArticlesById,
   getAllArticles,
-  getCommentsByArticleId, postCommentByArticleId, patchArticleById, deleteCommentById
+  getCommentsByArticleId,
+  postCommentByArticleId,
+  patchArticleById,
+  deleteCommentById,
 } = require("./controllers/articles.controllers");
 const {
   badUrlHandling,
@@ -15,23 +19,23 @@ const {
 } = require("./controllers/error.controllers.js");
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/api", getAllEndpoints)
+app.get("/api", getAllEndpoints);
 app.get("/api/topics", getTopics);
 app.get("/api/articles", getAllArticles);
 app.get("/api/articles/:article_id", getArticlesById);
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-app.get("/api/users", getUsers)
+app.get("/api/users", getUsers);
 
-app.patch("/api/articles/:article_id", patchArticleById)
+app.patch("/api/articles/:article_id", patchArticleById);
 
 app.post("/api/articles/:article_id/comments", postCommentByArticleId);
 
-app.delete("/api/comments/:comment_id", deleteCommentById)
-
-app.use(customErrorMiddleware);
-app.use(handlePsqlErrorsMiddleware);
+app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.all("/*", badUrlHandling);
+app.use(customErrorMiddleware);
+app.use(handlePsqlErrorsMiddleware);
 
 module.exports = app;
